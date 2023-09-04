ee67599fc35c4bef7a4aa8faf6b5a2a02f60b031
quantum0X <thevishal2108@gmail.com>
2023-09-04 17:07:20 +0530

    login, signup form created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState();
+  return (
+    <div>
+      <form>
+        <input
+          value={mail}
+          placeholder="example@xyz.com"
+          onChange={(e) => setMail(e.target.value)}
+        />
+        <input
+          value={password}
+          placeholder="********"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  return (
+    <div>
+      <form>
+        <input
+          value={mail}
+          placeholder="example@xyz.com"
+          onChange={(e) => setMail(e.target.value)}
+        />
+        <input
+          value={password}
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          value={confirmPassword}
+          placeholder="confirm password"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
