4fa4151e98a55d5380806df233e96d8ddb00e348
quantum0X <thevishal2108@gmail.com>
2023-09-09 16:43:25 +0530

    login & logout functionality added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [username, setUsername] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((res) => {
+      res.json().then((userInfo) => {
+        setUsername(userInfo.mail);
+      });
+    });
+  }, []);
+
+  const handleLogout = () => {
+    fetch("http://localhost:4000/logout", {
+      method: "POST",
+      credentials: "include",
+    });
+    setUsername(null);
+  };
+
   return (
     <div className="flex py-6 justify-between items-centersticky top-0 z-20">
       <div>
@@ -10,12 +29,25 @@ const Navbar = () => {
         </Link>
       </div>
       <div>
-        <Link className="mx-2 text-lg" to="/login">
-          Login
-        </Link>
-        <Link className="mx-2 text-lg" to="/signup">
-          Signup
-        </Link>
+        {username ? (
+          <>
+            <Link className="mx-2 text-lg" to="#">
+              New Post
+            </Link>
+            <button className="mx-2 text-lg" onClick={handleLogout}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <Link className="mx-2 px-4 py-1 text-lg outline" to="/login">
+              Login
+            </Link>
+            <Link className="mx-2 text-lg px-4 py-1 bg-gray-500" to="/signup">
+              Signup
+            </Link>
+          </>
+        )}
       </div>
     </div>
   );
