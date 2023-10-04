4facc816fd01143762f2edffcaba6b23efb3433d
quantum0X <thevishal2108@gmail.com>
2023-10-04 19:32:39 +0530

    signup UI update

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -34,27 +34,40 @@ const Signup = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="w-full h-80vh bg-slate-200 flex flex-col justify-center items-center">
+      <form className="w-72 flex flex-col m-auto" onSubmit={handleSubmit}>
+        <div className="py-2 px-10 my-3 bg-main-dark text-main-text-light border border-3 text-center border-gray-500 rounded-md hover:bg-main-hover cursor-pointer">
+          Sign in with Google
+        </div>
+        <hr className="h-[2px] w-11/12 my-4 bg-main-light mx-auto" />
+
         <input
+          className=" py-2 px-10 my-2 border border-3 border-main-light  rounded-md"
           value={mail}
           type="email"
           placeholder="example@xyz.com"
           onChange={(e) => setMail(e.target.value)}
         />
         <input
+          className=" py-2 px-10 my-2 border border-3 border-main-light  rounded-md"
           value={password}
           type="password"
           placeholder="password"
           onChange={(e) => setPassword(e.target.value)}
         />
         <input
+          className=" py-2 px-10 my-2 border border-3 border-main-light  rounded-md"
           value={confirmPassword}
           type="password"
           placeholder="confirm password"
           onChange={(e) => setConfirmPassword(e.target.value)}
         />
-        <button type="submit">Signup</button>
+        <button
+          className="bg-main-dark w-fit px-5 py-1 mx-auto text-lg text-main-text-light rounded-md font-medium hover:bg-main-hover"
+          type="submit"
+        >
+          Signup
+        </button>
       </form>
     </div>
   );
